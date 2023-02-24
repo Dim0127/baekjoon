c24f148303e2382397b70f7e350f0951ce60e56a
Dim0127 <github sjmrabbit@naver.com>
2023-02-24 23:46:39 +0900

    N과 M (1)

--- Silver/no15649.py
+++ Silver/no15649.py
@@ -8,11 +8,9 @@ def backtracking(k, m, permutation, check):
     else:
         for i in range(n):
             if check[i]==0:
-                check[i]=1
-                permutation[k]=i+1
+                check[i]=1; permutation[k]=i+1
                 backtracking(k+1,m,permutation, check)
-                check[i]=0
-                permutation[k]=0
+                check[i]=0; permutation[k]=0
     
 n, m=map(int,input().split())
 check=[0 for i in range(8)]
